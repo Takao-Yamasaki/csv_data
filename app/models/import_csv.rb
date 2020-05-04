80bb0014fe60596f03cfec1041a7950bf00a6767
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-05-04 19:19:53 +0900

    csvファイルのインポートを効率の良い書き方へ修正

--- app/models/import_csv.rb
+++ app/models/import_csv.rb
@@ -2,12 +2,14 @@ class ImportCsv < ApplicationRecord
     #CSVデータのパスを引数として受け取り、インポート処理をする
     #クラスメソッドの場合、new演算子がいらない
     def self.import(path)
+        list = []
         CSV.foreach(path,headers:true) do |row|
-            User.create(
+            list << {
                 name: row["name"],
                 age: row["age"],
                 address: row["address"]
-            )
+            }
+            User.create(list)
         end
     end
 end
