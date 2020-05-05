d27e603bffd4b64981acc9dd4e928f2e19d97a59
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-05-05 14:46:59 +0900

    インポート機能のコード改善

--- app/models/import_csv.rb
+++ app/models/import_csv.rb
@@ -1,7 +1,22 @@
+# class ImportCsv < ApplicationRecord
+#     #CSVデータのパスを引数として受け取り、インポート処理をする
+#     #クラスメソッドの場合、new演算子がいらない
+#     def self.import(path)
+#         list = []
+#         CSV.foreach(path,headers:true) do |row|
+#             list << {
+#                 name: row["name"],
+#                 age: row["age"],
+#                 address: row["address"]
+#             }
+#             User.create(list)
+#         end
+#     end
+# end
+
 class ImportCsv < ApplicationRecord
-    #CSVデータのパスを引数として受け取り、インポート処理をする
-    #クラスメソッドの場合、new演算子がいらない
-    def self.import(path)
+    def self.users_data
+        path = File.join Rails.root,"db/csv/csv_data.csv"
         list = []
         CSV.foreach(path,headers:true) do |row|
             list << {
@@ -9,7 +24,13 @@ class ImportCsv < ApplicationRecord
                 age: row["age"],
                 address: row["address"]
             }
-            User.create(list)
+        end
+        puts "インポート処理を開始"
+        begin
+            User.create!(list)
+            puts "インポート完了！！"
+        rescue ActiveModel::UnknownAttributeError => invalid
+            puts "インポートに失敗：UnknownAttributeError"
         end
     end
-end
+end 
